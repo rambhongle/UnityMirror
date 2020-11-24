fcb6be1705644aa6dd07cdd64ba604d6468dfad9
Ram Bhongle <bhongleram@gmail.com>
2020-11-24 18:33:13 +0530

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private void OnDestroy() {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake() 
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(true)
+        {
+         
+        }
+    }
+}
