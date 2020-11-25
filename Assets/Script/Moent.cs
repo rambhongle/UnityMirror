4feb5ddbe1e9a9680b62bb673aaaa04b9d01284a
Ram Bhongle <bhongleram@gmail.com>
2020-11-25 13:30:51 +0530

    Second

---
+++ Assets/Script/Moent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+
+public class Moent : NetworkBehaviour
+{
+    
+    void PlayerMOM()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 mov = new Vector3(h, v, 0);
+        transform.position = transform.position + mov;
+    }
+
+    private void Update()
+    {
+        PlayerMOM();
+    }
+}
